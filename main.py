81ae26da21250224560c0d696135fdedc3eb0c18
samgonz <doomteam1@gmail.com>
2022-10-12 22:08:56 -0500

    additional direction changes

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 from turtle import Turtle, Screen, onkey
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.color('Blue')
+tommy_the_turtle = Turtle()
+tommy_the_turtle.color('Red')
 screen = Screen()
 
 def up():
-    timmy_the_turtle.setheading(90)
-    timmy_the_turtle.forward(100)
+    timmy_the_turtle.forward(10)
  
  
 def down():
-    timmy_the_turtle.setheading(270)
-    timmy_the_turtle.forward(100)
+    timmy_the_turtle.backward(10)
  
  
 def left():
-    timmy_the_turtle.setheading(180)
-    timmy_the_turtle.forward(100)
- 
+    new_heading = timmy_the_turtle.heading() + 10
+    timmy_the_turtle.setheading(new_heading)
  
 def right():
-    timmy_the_turtle.setheading(0)
-    timmy_the_turtle.forward(100)
+    new_heading = timmy_the_turtle.heading() - 10
+    timmy_the_turtle.setheading(new_heading)    
     
+def clear_timmy():
+    timmy_the_turtle.clear()
+    
+def goHome():
+    timmy_the_turtle.home()
+    timmy_the_turtle.clear()
+
     
 screen.listen()
 onkey(up, 'Up')
 onkey(down, 'Down')
 onkey(left, 'Left')
 onkey(right, 'Right')
+onkey(clear_timmy, 'space')
+onkey(goHome, 'h')
 screen.exitonclick()
     
     
